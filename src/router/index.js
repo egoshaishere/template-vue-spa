305d4fcd23c8737b125d4fd75ebf3c31ec209609
Егор <eremin.e@m-style.spb.ru>
2022-08-12 15:03:38 +0300

    add app

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
-import Exchange from '../views/Exchange.vue'
-import Charts from '../views/Charts.vue'
 import NotFound from '../views/404.vue'
-import MultiChartView from '../views/MultiChartView.vue'
 import Sandbox from '../views/Sandbox.vue'
 
 Vue.use(VueRouter)
@@ -14,21 +11,6 @@ const routes = [
     path: '/',
     name: 'dashboard',
     component: Dashboard
-  },
-  {
-    path: '/exchange',
-    name: 'exchange',
-    component: Exchange
-  },
-  {
-    path: '/charts',
-    name: 'charts',
-    component: Charts
-  },
-  {
-    path: '/multicharts',
-    name: 'multicharts',
-    component: MultiChartView
   },
   {
     path: '/sandbox',
--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
     <v-btn v-on:click="click">test</v-btn>
+
+
+    <div class="test" v-if="prz"></div>
   </div>
 </template>
 
 <script>
 export default {
     name: "Sandbox",
+    data() {
+      return {
+        prz: false
+
+      }
+    },
     methods: {
         click(){
             console.log('uipg')
+            console.log(this.prz)
+            this.prz = !this.prz
         }
     }
 }
 </script>
 
 <style>
+
+.test {
+  border: solid 2px red;
+  width: 300px;
+  height: 150px;
+}
 
 </style>
