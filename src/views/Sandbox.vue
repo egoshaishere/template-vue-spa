0c611e813cf9826bb5cddfbfa7f27afe9ab682e3
Егор <eremin.e@m-style.spb.ru>
2022-08-17 13:33:10 +0300

    check some

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -2,6 +2,7 @@
   <div>
     <v-btn v-on:click="click">test</v-btn>
 
+    <v-btn v-on:click="clickTest">check watch</v-btn>
 
     <div class="test" v-if="prz"></div>
   </div>
@@ -9,29 +10,38 @@
 
 <script>
 export default {
-    name: "Sandbox",
-    data() {
-      return {
-        prz: false
-
-      }
+  name: "Sandbox",
+  data() {
+    return {
+      prz: false,
+      test: false,
+    };
+  },
+  methods: {
+    click() {
+      console.log("uipg");
+      console.log(this.prz);
+      this.prz = !this.prz;
     },
-    methods: {
-        click(){
-            console.log('uipg')
-            console.log(this.prz)
-            this.prz = !this.prz
-        }
-    }
-}
+    clickTest() {
+      this.test = !this.test;
+    },
+  },
+  watch: {
+    prz() {
+      console.log("watcher prz is triggered");
+    },
+    test() {
+      console.log("watcher test is triggered");
+    },
+  },
+};
 </script>
 
 <style>
-
 .test {
   border: solid 2px red;
   width: 300px;
   height: 150px;
 }
-
 </style>
